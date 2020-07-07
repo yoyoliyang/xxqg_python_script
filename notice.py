b1fa6fb564c65013efb375495d869b14fdf7da1c
yoyoliyang@gmail.com <yoyoliyang@gmail.com>
2020-07-07 13:47:11 +0800

    利用requests发送请求

--- notice.py
+++ notice.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
 import os
+import requests
+
+# IFTTT APP发送通知函数，注意修改自己的key
 
 
 def send(message):
     event_name = 'iphone_notice'
     key = ''
-    data = f'{{\\"value1\\" : \\"{message}\\"}}'
-    # print(data)
-    os.system(
-        f'curl -H "Content-type: application/json" -X POST -d "{data}"  https://maker.ifttt.com/trigger/{event_name}/with/key/{key}')
+    url = f'https://maker.ifttt.com/trigger/{event_name}/with/key/{key}'
+    req = requests.post(url, json={"value1": message})
     print(f'sending IFTTT message: {event_name}')
+    if req.status_code == 200:
+        print('已发送通知')
+    else:
+        print('error ' + req.status_code)
